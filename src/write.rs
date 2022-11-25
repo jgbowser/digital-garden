cd4ff85fe919da7b870efcc58fa5f5d69ac66d79
John Bowser <john.bow7720@gmail.com>
2022-11-24 21:44:47 -0700

    bring in the edit crate and make temp file creation more user friendly

--- src/write.rs
+++ src/write.rs
@@ -1,7 +1,29 @@
-use color_eyre::Result;
+use color_eyre::eyre::{Result, WrapErr};
+use edit::{edit_file, Builder};
+use std::io::{Read, Write};
 use std::path::PathBuf;
 
-pub fn write(garden_path: PathBuf, title: Option<String>) -> Result<()> {
-    dbg!(garden_path, title);
+const TEMPLATE: &[u8; 2] = b"# ";
+
+pub fn write(
+    garden_path: PathBuf, 
+    title: Option<String>
+) -> Result<()> {
+    let (mut file, filepath) = Builder::new()
+        .suffix(".md")
+        .rand_bytes(5)
+        .tempfile_in(&garden_path)
+        .wrap_err("Failed to create wip file")?
+        .keep()
+        .wrap_err("Failed to keep tempfile")?;
+    file.write_all(TEMPLATE)?;
+    // let the user write whatever they want in their favorite editor
+    // before returning to the cli and finishing up
+    edit_file(filepath)?;
+    // Read the user's changes back from th file into a string
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+
+    dbg!(contents);
     todo!()
 }
