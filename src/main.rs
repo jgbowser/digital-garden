1f924211bcccf00e3dc8b4453bccceb7bfee2a0a
John Bowser <john.bow7720@gmail.com>
2022-11-24 20:32:46 -0700

    add ability to specify a directory for the garden

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
-use color_eyre::eyre::Result;
+use std::path::PathBuf;
+
+use color_eyre::eyre::{eyre, Result, WrapErr};
 use digital_garden::write;
+use directories::UserDirs;
 use structopt::StructOpt;
 
 /// A CLI for growing and curation of a digital garden
 #[derive(StructOpt, Debug)]
 #[structopt(name = "garden")]
 struct Opt {
+    #[structopt(parse(from_os_str), short = "p", long, env)]
+    garden_path: Option<PathBuf>,
+
     #[structopt(subcommand)]
     cmd: Command,
 }
@@ -13,21 +19,31 @@ struct Opt {
 #[derive(StructOpt, Debug)]
 enum Command {
     /// write something in your garden
-    /// 
+    ///
     /// This command will open your $EDITOR, wait for you
     /// to write something, and then save the file to your
     /// garden
-    Write { 
+    Write {
         #[structopt(short, long)]
         /// Optionally set a title for what you are going to write about
-        title: Option<String> },
+        title: Option<String>,
+    },
+}
+
+fn get_default_garden_dir() -> Result<PathBuf> {
+    let user_dirs = UserDirs::new().ok_or_else(|| eyre!("Could not find home directory"))?;
+    Ok(user_dirs.home_dir().join(".garden"))
 }
 
 fn main() -> Result<()> {
     color_eyre::install()?;
     let opt = Opt::from_args();
     dbg!(&opt);
+    let garden_path = match opt.garden_path {
+        Some(pathbuf) => Ok(pathbuf),
+        None => get_default_garden_dir().wrap_err("`garden_path` was not supplied"),
+    }?;
     match opt.cmd {
-        Command::Write { title } => write(title),
+        Command::Write { title } => write(garden_path, title),
     }
 }
