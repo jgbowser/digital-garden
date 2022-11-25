51b09dfbc8673da95748261ba3dd0c2e14557c1a
John Bowser <john.bow7720@gmail.com>
2022-11-25 11:15:42 -0700

    unskip and update test_write test

--- tests/integration.rs
+++ tests/integration.rs
@@ -1,4 +1,6 @@
 use assert_cmd::Command;
+use assert_fs::prelude::*;
+use predicates::prelude::*;
 use color_eyre::eyre::Result;
 
 #[test]
@@ -19,9 +21,28 @@ fn test_write_help() -> Result<()> {
 }
 
 #[test]
-#[ignore]
 fn test_write() {
+    let temp_dir = assert_fs::TempDir::new().unwrap();
+
     let mut cmd = Command::cargo_bin("garden").unwrap();
-    let assert = cmd.arg("write").assert();
+    let fake_editor_path = std::env::current_dir()
+        .expect("expect to be in a dir")
+        .join("tests")
+        .join("fake_editor.sh");
+    if !fake_editor_path.exists() {
+        panic!("fake editor shell script not found")
+    }
+    let assert = cmd
+        .env("EDITOR", fake_editor_path.into_os_string())
+        .env("GARDEN_PATH", temp_dir.path())
+        .arg("write")
+        .arg("-t")
+        .arg("atitle")
+        .write_stdin("N\n".as_bytes())
+        .assert();
+
     assert.success();
+
+    temp_dir.child("atitle.md")
+    .assert(predicate::path::exists());
 }
